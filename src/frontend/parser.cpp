672252ae966b7906081392f7cf0c4d80a1720842
twopic2 <bowenzhang0@gmail.com>
2025-12-27 23:47:54 -0800

    fixed merge issues

--- src/frontend/parser.cpp
+++ src/frontend/parser.cpp
@@ -340,14 +340,10 @@ std::unique_ptr<Ast::ast_node> Parser::parser_class::parse_function_def() {
         consume(Token::token_type::IDENTIFIER);
     }
 
-    consume(Token::token_type::LPAREN);
-<<<<<<< HEAD:src/parser.cpp
-    
-=======
+    consume(Token::token_type::LPAREN);    
     if (!match(Token::token_type::INDENT)) {
         throw std::runtime_error(std::format("There is no Indent at source:{}", current_token().line));
     }
->>>>>>> pr-1:src/frontend/parser.cpp
 
     auto param_list = std::make_unique<Ast::ast_node>(Ast::node_type::PARAMETER_LIST,
                                                        "",
